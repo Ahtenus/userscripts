2ee7369ae0ec571d3a3763e2191849b0bc1b43c6
Viktor Barsk <543514+Ahtenus@users.noreply.github.com>
2020-04-18 10:53:20 +0200

    Create hackernews.user.js

---
+++ hackernews.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Hacker News
+// @namespace    https://barsk.dev/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://news.ycombinator.com/item*
+// @grant        none
+// @updateURL   https://raw.githubusercontent.com/Ahtenus/userscripts/master/hackernews.user.js
+// @downloadURL https://raw.githubusercontent.com/Ahtenus/userscripts/master/hackernews.user.js
+// ==/UserScript==
+
+(function() {
+    function addGlobalStyle(css) {
+        var head, style;
+        head = document.getElementsByTagName('head')[0];
+        if (!head) { return; }
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = css.replace(/;/g, ' !important;');
+        head.appendChild(style);
+    }
+    'use strict';
+    addGlobalStyle(".togg { font-size: 2em; }");
+})();
