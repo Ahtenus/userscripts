9c4dbd09092d54c4b9e942379772cedde7d15117
Viktor Barsk <git@viktorbarsk.se>
2020-04-25 16:31:19 +0200

    Added update.sh

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+changedFiles=$(git status --porcelain | awk 'BEGIN { ORS=" " }; { print $2 }')
+version=1.$(date -u +"%Y%m%d.%H%M%S")
+sed -i '' "/.*version.*/s/1\.[0-9]*\.[0-9]*/$version/g" $changedFiles
+
+git commit -am "Updated $changedFiles"
+git push
