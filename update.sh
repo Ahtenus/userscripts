e634a9219fe29fd88c54d5fd4feafa0225b43a11
Viktor Barsk <git@viktorbarsk.se>
2020-04-25 16:55:18 +0200

    Updated update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 changedFiles=$(git status --porcelain | awk 'BEGIN { ORS=" " }; { print $2 }')
-version=1.$(date -u +"%Y%m%d.%H%M%S")
+version=1.$(date +"%Y%m%d.%H%M%S")
 sed -i '' "/.*version.*/s/1\.[0-9]*\.[0-9]*/$version/g" $changedFiles
 
 git commit -am "Updated $changedFiles"
