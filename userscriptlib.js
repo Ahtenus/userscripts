31504c70d12a5fba931e6c25fb9a824d640bcb57
Viktor Barsk <git@viktorbarsk.se>
2020-04-25 17:09:55 +0200

    Updated userscriptlib.js

---
+++ userscriptlib.js
@@ -0,0 +1,21 @@
+const userscipt = {
+    globalStyle: (css) => {
+        var head, style;
+        head = document.getElementsByTagName('head')[0];
+        if (!head) { return; }
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = css.replace(/;/g, ' !important;');
+        head.appendChild(style);
+    },
+    whenTrue: (predicate) => new Promise((resolve, reject) => {
+        const checkPredicate = () => {
+            if(predicate()) {
+                resolve()
+            } else {
+                setTimeout(checkPredicate, 10);
+            }
+        }
+        checkPredicate()
+    })
+}
