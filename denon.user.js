340f4df42e62b75ad472d8fdc208e65e9d2faafe
Viktor Barsk <git@viktorbarsk.se>
2020-04-25 13:23:11 +0200

    Denon user script

--- denon.user.js
+++ denon.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Denon
 // @namespace    https://barsk.dev/
-// @version      0.9
+// @version      0.10
 // @description  Improvements to Denon AVR-X1300W web GUI
 // @author       Ahtenus
 // @match        http://10.0.0.100/MainZone/index.html
